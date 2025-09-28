2c5858bc8db21ed472b2195dfda4751852ad806e
Hamza Maqsood <hamzamaqsood072000@gmail.com>
2025-09-28 19:45:50 +0500

    made db variables env

--- db/data-source.ts
+++ db/data-source.ts
@@ -3,11 +3,11 @@ import { DataSource, DataSourceOptions } from "typeorm";
 
 export const dataSourceOptions:DataSourceOptions = {
     type: 'postgres',
-    host: "localhost",
-    port:  5432,
-    username: "postgres",
-    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: Number.parseInt(process.env.DB_PORT || '5432'),
     database: "pharmacy",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
     entities:["dist/**/*.entity.js"],
     migrations:["dist/db/migrations/*.js"],
     synchronize:true
